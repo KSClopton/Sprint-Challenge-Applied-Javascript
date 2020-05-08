387326aca973e99df3c75e4b0edfdb00c61c7ad6
KSClopton <kyle.clopton@outlook.com>
2020-05-08 12:20:26 -0400

    Mostly finsihed with tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,49 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+function makeTabs(attrs){
+
+// create elements
+const topicTitle = document.querySelector('.title')
+const divTab1 = document.createElement('div')
+const divTab2 = document.createElement('div')
+const divTab3 = document.createElement('div')
+const divTab4 = document.createElement('div')
+const divTab5 = document.createElement('div')
+
+// append to the existing title class
+topicTitle.appendChild(divTab1)
+topicTitle.appendChild(divTab2)
+topicTitle.appendChild(divTab3)
+topicTitle.appendChild(divTab4)
+topicTitle.appendChild(divTab5)
+// add classes
+divTab1.classList.add('tabs')
+divTab2.classList.add('tabs')
+divTab3.classList.add('tabs')
+divTab4.classList.add('tabs')
+divTab5.classList.add('tabs')
+
+// add text content
+divTab1.textContent = attrs
+divTab2.textContent = attrs
+divTab3.textContent = attrs
+divTab4.textContent = attrs
+divTab5.textContent = attrs
+
+return topicTitle
+}
+
+function getLambdaTimes(){
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+        .then(response => {
+            const divTopics = document.querySelector('.topics')
+            topics = response.data
+            lambdaTimes = makeTabs(topics)
+            console.log(lambdaTimes)
+            divTopics.appendChild(lambdaTimes)
+            
+        })
+}
+getLambdaTimes()
